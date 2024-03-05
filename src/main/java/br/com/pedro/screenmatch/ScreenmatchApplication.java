3175a0ec6418ed34f745a7189d528b4e1c94921a
pedro bovo <pedrobovo123@gmail.com>
2024-03-05 12:27:54 -0300

    Primeiro projeto usando Spring

---
+++ src/main/java/br/com/pedro/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.pedro.screenmatch;
+
+import br.com.pedro.screenmatch.model.DadosSerie;
+import br.com.pedro.screenmatch.service.ConsumoApi;
+import br.com.pedro.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=breaking+bad&apikey=ca1dce10");
+		System.out.println(json);
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obeterDados(json, DadosSerie.class);
+
+		System.out.println(dados);
+
+	}
+}
---
+++ src/main/java/br/com/pedro/screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package br.com.pedro.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao ){
+
+}
